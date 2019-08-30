37c162347a8e41a80bddc2a7fc6bc690e30808eb
jiwei <13165782821@163.com>
2019-08-31 00:18:14 +0800

    项目前后端分离架构

--- gmall-api/src/main/java/com/igeekhome/gmall/service/SpuService.java
+++ gmall-api/src/main/java/com/igeekhome/gmall/service/SpuService.java
@@ -2,9 +2,12 @@ package com.igeekhome.gmall.service;
 
 import com.igeekhome.gmall.bean.PmsBaseAttrInfo;
 import com.igeekhome.gmall.bean.PmsProductInfo;
+import com.igeekhome.gmall.bean.PmsProductSaleAttr;
 
 import java.util.List;
 
 public interface SpuService {
     List<PmsProductInfo> spuList(String catalog3Id);
+
+    List<PmsProductSaleAttr> spuSaleAttrList(String productId);
 }
